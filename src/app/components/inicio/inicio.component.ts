41c1032c62c510ebdb4e650eb98049fb6ec92180
Emiliano Rago <emiliano.rg93@gmail.com>
2019-11-13 18:01:26 -0300

    agregando animaciones

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: "app-inicio",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.css"]
 })
 export class InicioComponent implements OnInit {
+  variable: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  cambiarEstado() {
+    if(this.variable == false){
+      this.variable = true;
+    }else{
+      this.variable = false;
+    }
   }
 
+  ngOnInit() {}
 }
